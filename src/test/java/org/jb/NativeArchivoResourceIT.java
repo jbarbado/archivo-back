9f0aac71a1444be965ccdbf8ad446330d1a2d27d
Javier BARBADO <jbarbado@norsys.fr>
2019-09-24 17:04:01 +0200

    initial commit

---
+++ src/test/java/org/jb/NativeArchivoResourceIT.java
@@ -0,0 +1,9 @@
+package org.jb;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeArchivoResourceIT extends ArchivoResourceTest {
+
+    // Execute the same tests but in native mode.
+}
